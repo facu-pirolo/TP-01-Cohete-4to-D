7c47c5ea5b48642be408ff0599e11de048fc284a
facu-pirolo <49518308@est.ort.edu.ar>
2025-03-27 10:29:34 -0300

    hacer despegar

--- Assets/Scripts/BotonScript.cs
+++ Assets/Scripts/BotonScript.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class BotonScript : MonoBehaviour
 {
     public GameObject botonGO;
+    public CoheteScript coheteScript;  //script del cohete
 
     // Start is called before the first frame update
     void Start()
@@ -21,6 +22,7 @@ public class BotonScript : MonoBehaviour
     void OnMouseDown()
     {
         botonGO.SetActive(false);
+        coheteScript.Despegue();
     }
 
     void OnMouseUp()
